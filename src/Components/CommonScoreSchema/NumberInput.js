e2063072f02779e99524b5242a9fcac3082b7bf1
Budvin Chathura <chathura97rc@gmail.com>
2020-03-30 22:02:08 +0530

    scoring schema input added

---
+++ src/Components/CommonScoreSchema/NumberInput.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import TextField from '@material-ui/core/TextField';
+import { useSelector, useDispatch } from 'react-redux';
+
+
+
+export default function NumberInput(props) {
+    const dispatch = useDispatch();
+    const ms = useSelector(state => state[props.inputSelector]);
+
+    const [errorStatus, setErrorStatus] = React.useState(false);    
+
+    const onInputChange = (event) => {
+        const val = Math.floor(Number(event.target.value))
+        if(val){
+            if(props.acceptedType==='1'){
+                if(val>0){
+                    dispatch(props.inputHandler(val));
+                    setErrorStatus(false);
+                }else{
+                    dispatch(props.inputHandler(event.target.value))
+                    setErrorStatus(true);
+                }
+            }
+            else if(props.acceptedType==='-1'){
+                if(val<0){
+                    dispatch(props.inputHandler(val));
+                    setErrorStatus(false);
+                }else{
+                    dispatch(props.inputHandler(event.target.value))
+                    setErrorStatus(true);
+                }
+            }
+        }else{
+            dispatch(props.inputHandler(event.target.value))
+            setErrorStatus(true);
+        }
+
+        
+    }
+
+    return (
+
+        <div>
+            <TextField
+                error={errorStatus}
+                label={props.label}
+                value={ms}
+                helperText={errorStatus? props.helperText:''}
+                variant="outlined"
+                type="number"
+                onChange={onInputChange}
+            />
+        </div>
+    );
+}
