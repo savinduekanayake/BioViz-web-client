abcf98a472c75517fb278fac865b681c7243b167
Budvin Chathura <chathura97rc@gmail.com>
2020-03-31 21:13:34 +0530

    PairAlign alignment display added

---
+++ src/Components/PairAlign/PairAlignAlignment.js
@@ -0,0 +1,71 @@
+import React from 'react'
+import { Avatar } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+
+const algnA = "GATA-CTACTCAGTATTCTACCACCA-ACGAT-"
+const algnB = "GACATCTA-T-AG-A--ATACGAATATACGATA"
+
+
+const useStyles = makeStyles(theme => ({
+    avatar: {
+        height:'20px',
+        width:'20px',
+        fontSize:'12px',
+        
+    },
+    A:{
+        color:'white',
+        backgroundColor:'red',
+    },
+    C:{
+        color:'white',
+        backgroundColor:'blue',
+    },
+    G:{
+        color:'white',
+        backgroundColor:'purple',
+    },
+    T:{
+        color:'white',
+        backgroundColor:'green',
+    },
+    ga:{
+        color:'black',
+        backgroundColor:'black',
+    }
+    
+}))
+
+export default function PairAlignAlignment() {
+    const classes = useStyles();
+
+    let row1 = [];
+    for (var i = 0; i < algnA.length; i++) {
+        const char = algnA.charAt(i)==='-'?'ga':algnA.charAt(i);
+        row1.push(<td><Avatar variant="square" className={`${classes.avatar} ${classes[char]}`}>{algnA.charAt(i)}</Avatar></td>)
+    }
+    let row2 = [];
+    for (var j = 0; j < algnB.length; j++) {
+        const char = algnB.charAt(j)==='-'?'ga':algnB.charAt(j);
+        row2.push(<td><Avatar variant="square" className={`${classes.avatar} ${classes[char]}`}>{algnB.charAt(j)}</Avatar></td>)
+    }
+
+    let middle=[];
+    for (var k = 0; k < algnA.length; k++) {
+        const char = algnA.charAt(k)===algnB.charAt(k) && algnA.charAt(k)!=='-'? "\u007C" :''; 
+
+        middle.push(<td><b>{char}</b></td>)
+    }
+
+    return (
+        <div>
+            <table>
+                <tr>{row1}</tr>
+                <tr>{middle}</tr>
+                <tr>{row2}</tr>
+            </table>
+
+        </div>
+    )
+}
--- src/Components/PairAlign/PairAlignContent.js
+++ src/Components/PairAlign/PairAlignContent.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import PairAlignInput from './PairAlignInput'
 import Matrix from '../Matrix/Matrix'
+import PairAlignAlignment from './PairAlignAlignment'
+import { Button } from '@material-ui/core'
 
 export default function PairAlignContent() {
     return (
@@ -8,7 +10,11 @@ export default function PairAlignContent() {
             <h2>PairAlign Mode</h2>
             <PairAlignInput/>
             <br/>
+            <Button variant="outlined" color="secondary">Submit</Button>
+            <br/>
             <Matrix/>
+            <br/>
+            <PairAlignAlignment/>
         </div>
     )
 }
