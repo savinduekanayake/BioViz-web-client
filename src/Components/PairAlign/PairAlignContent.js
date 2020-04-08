19df33a0b7a2c9336a3ee182d7b5aac2b8ad8698
Budvin Chathura <chathura97rc@gmail.com>
2020-04-08 11:21:12 +0530

    onsubmit callback

--- src/Components/PairAlign/PairAlignContent.js
+++ src/Components/PairAlign/PairAlignContent.js
@@ -7,19 +7,23 @@ import Button from '@material-ui/core/Button';
 export default function PairAlignContent() {
     const [submitted, setSubmitted] = React.useState(false);
 
-    const onSubmit=()=>{
+    const onSubmit = () => {
         setSubmitted(true);
     };
     return (
         <div>
             <h2>PairAlign Mode</h2>
-            <PairAlignInput/>
-            <br/>
-            <Button variant="outlined" color="secondary" onClick={onSubmit}>Submit</Button>
-            <br/>
-            <Matrix/>
-            <br/>{submitted?<PairAlignAlignment/>:''}
-            {/* <PairAlignAlignment/> */}
+            <PairAlignInput />
+            <br />
+            <Button
+                variant="outlined"
+                color="secondary"
+                onClick={onSubmit}>
+                Submit
+            </Button>
+            <br />
+            <Matrix />
+            <br />{submitted ? <PairAlignAlignment /> : ''}
         </div>
     );
 }
