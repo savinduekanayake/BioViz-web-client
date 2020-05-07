047884a49ccb0ccf1b34134620353b763824edcb
Savindu Kalsara Ekanayake <47809365+savinduekanayake@users.noreply.github.com>
2020-05-08 03:20:39 +0530

    added HomeSection testings

--- src/Components/HomeSection/HomeSection.js
+++ src/Components/HomeSection/HomeSection.js
@@ -19,7 +19,6 @@ export default function HomeSection() {
             {/* <Test /> */}
             {/* <Game /> */}
             <DialogScreen />
-            {/* <SingleService /> */}
         </div>
     );
 }
