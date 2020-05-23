2113a6c0edebb3cba0bf1d48ee5ffee2ec5b51a8
Budvin Chathura <chathura97rc@gmail.com>
2020-05-23 16:15:11 +0530

    MSA result ui refactor

--- src/Components/MSA/MSAInput.js
+++ src/Components/MSA/MSAInput.js
@@ -9,7 +9,9 @@ import {Box} from '@material-ui/core';
 export default function MSAInput() {
     return (
         <div>
-            <MSASequencesInput/>
+            <Box boxShadow={3} padding={5}>
+                <MSASequencesInput/>
+            </Box>
             <MSAAlgoSelector/>
             <Box boxShadow={3} marginTop={7} marginBottom={7} padding={5}>
                 <CommonScore />
