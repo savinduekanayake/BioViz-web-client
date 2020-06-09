2da37e9c683c9472fa1f9ace34805a70e29bbfec
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-06-09 22:59:08 +0000

    Deploy budvinchathura/BioViz-web-client to github.com/budvinchathura/BioViz-web-client.git:gh-pages

---
+++ precache-manifest.5a597416b42a2feecb82333090dfe9a5.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd883485d7c6c8e1f1e53e28b6c90582",
+    "url": "/BioViz-web-client/index.html"
+  },
+  {
+    "revision": "69d8cae57cc8b8611b1d",
+    "url": "/BioViz-web-client/static/css/main.fda87b3f.chunk.css"
+  },
+  {
+    "revision": "f76351fa987a3a3bf8b4",
+    "url": "/BioViz-web-client/static/js/2.2f6caff3.chunk.js"
+  },
+  {
+    "revision": "4f3ee35d4ae1279a07ca6ba39f640448",
+    "url": "/BioViz-web-client/static/js/2.2f6caff3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69d8cae57cc8b8611b1d",
+    "url": "/BioViz-web-client/static/js/main.64f21956.chunk.js"
+  },
+  {
+    "revision": "71c0809e4a65b0e9bab2",
+    "url": "/BioViz-web-client/static/js/runtime-main.d97b5dd1.js"
+  },
+  {
+    "revision": "eff3baf80c34d3f9455d22f0d4f2e7ea",
+    "url": "/BioViz-web-client/static/media/1.eff3baf8.jpg"
+  },
+  {
+    "revision": "a44f7dee3d1590eb946b1a9db0e8cc40",
+    "url": "/BioViz-web-client/static/media/2.a44f7dee.jpg"
+  },
+  {
+    "revision": "7d29872643dc1b29353691663384810e",
+    "url": "/BioViz-web-client/static/media/3.7d298726.png"
+  },
+  {
+    "revision": "f1cd5369c62534ab4724bfdf749db601",
+    "url": "/BioViz-web-client/static/media/MSA.f1cd5369.png"
+  },
+  {
+    "revision": "701c38d52f54508bcf0604c7367e635a",
+    "url": "/BioViz-web-client/static/media/dna.701c38d5.svg"
+  },
+  {
+    "revision": "f98482e500bbb0318b580c2b32dcd0ff",
+    "url": "/BioViz-web-client/static/media/game.f98482e5.jpg"
+  },
+  {
+    "revision": "26d4019477ed2062e1e3e0ad53a7dcc2",
+    "url": "/BioViz-web-client/static/media/pw.26d40194.png"
+  }
+]);
